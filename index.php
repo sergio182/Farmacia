a8bac62cf5538e8103b2a7c03ab54ef4149677d5
SergioCadenaLopez <62250981+sergio182@users.noreply.github.com>
2021-05-29 00:08:56 -0400

    Add files via upload
    pagina de login inicio de sesion v0.1

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Archivos CSS Bootstrap 4.4 -->
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Archivos CSS Personalizados -->
+    <link type="text/css" rel="stylesheet" href="css/estilos.css">
+    <title>Login</title>
+</head>
+
+<body class="bg-light ">
+
+
+
+    <div class="login-caja">
+        <img src="images/farma.jpg" alt="" class="avatar">
+            <p class="h3 text-md-center">Farmacia</p>
+            <form action="" method="post" accept-charset="utf-8">
+            <!--Usuario-->
+              <label for="username">Usuario</label>
+              <input type="text" name="nombre" placeholder="Ingrese Ususario">
+       
+              <label for="password">Contraseña</label>
+              <input type="password" name="contra" placeholder="Ingrese Password"name="password" id="password" class="txtfield" tabindex="2">
+        
+              <input type="submit" class="btn btn-danger text-left pl-4 " value="
+              Ingresar">
+              <a href="#">Olividaste tu contraseña</a>
+              
+        </form>
+
+    </div>
+
+
+
+
+
+
+
+
+
+    <!-- Archivos Bootstrap JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/popper.min.js"></script>
+</body>
+
+</html>
